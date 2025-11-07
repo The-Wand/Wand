1d40628db27b9f7cf143debea185869a1ce2d069
Alex Kozin <al@el-machine.com>
2025-11-07 16:29:54 +0900

    Add default test

---
+++ Tests/WandTests/Unit/DefaultTests.swift
@@ -0,0 +1,20 @@
+//
+//  KoraTalkTests.swift
+//  KoraTalkTests
+//
+//  Created by Aleksander Kozin on 4/11/25.
+//
+
+import Testing
+
+struct KoraTalkTests {
+
+    @Test
+    func launch()
+    async
+    throws
+    {
+
+    }
+
+}
