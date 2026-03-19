be9b194a84fb6ac939ab67c635fe6d0571f77aa8
Alex Kozin <al@el-machine.com>
2026-03-19 22:48:17 +0700

    Update dependency to from

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/The-Wand/Any.git", .upToNextMajor(from: "1.0.3") )
+        .package(url: "https://github.com/The-Wand/Any.git", .from: "1.0.3")
     ],
 
     targets: [
